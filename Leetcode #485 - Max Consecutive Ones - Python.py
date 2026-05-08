edff98c2c7ead0242f9f6ed96c8b8c9510b1173c
Solomiya_Les <solomiya.lesivtsiv@gmail.com>
2026-05-08 19:48:35 +0300

    Add a code Leetcode #485 to the folder Sorting

---
+++ Leetcode #485 - Max Consecutive Ones - Python.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: list[int]) -> int:
+        ans = 0
+        currentCount = 0
+
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                currentCount = 0
+            else:
+                currentCount += 1
+            
+            if currentCount > ans:
+                ans = currentCount
+                        
+        return ans
